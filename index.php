a1bfa7c13537ec1d27a04007d98326946c520afa
Rene Leveille <reneb.leveille@gmail.com>
2017-05-22 18:44:20 -0400

    displaying custom posts

--- index.php
+++ index.php
@@ -12,9 +12,21 @@
             </div>
             <div class="col span_1_of_3">
             <?php
+                echo '<h2>Countries</h2>';
                 $args = array(
-                    'posts_per_page'=>10,
-                    'post_type'=>array('posts','page')
+                    'post_type'=>array('countries'),
+                    'posts_per_page'=>10
+                );
+                $post_data = get_posts($args);
+                foreach($post_data as $post_item)
+                {
+                    echo get_the_title($post_item).'<hr/>';
+                }
+
+                echo '<h2>Posts and Pages</h2>';
+                $args = array(
+                    'post_type'=>array('post','page'),
+                    'posts_per_page'=>10
                 );
                 $post_data = get_posts($args);
                 foreach($post_data as $post_item)
