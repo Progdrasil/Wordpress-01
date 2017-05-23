9a9bb7a95b11403f4adfd1f231768bdb2713519b
Rene Leveille <reneb.leveille@gmail.com>
2017-05-22 20:45:45 -0400

    displaying post content

--- single.php
+++ single.php
@@ -8,12 +8,22 @@
     <body>
         <div class="section group">
             <div class="col span_3_of_3">
-            <?php echo'<h1>'.get_the_title().'</h1>';?>
+            <?php 
+                echo '<h1>'.get_the_title().'</h1>';
+                echo '<h4>'.get_the_date().'</h4>';
+            ?>
             </div>
         </div>
         <div class="section group">
             <div class="col span_2_of_3">
-            This is column 1 and 2
+            <?php
+                $posts = get_posts();
+                foreach($posts as $post)
+                {
+                    setup_postdata($post);
+                    echo get_the_content();
+                }
+            ?>
             </div>
             <div class="col span_1_of_3">
             <?php get_sidebar(); ?>
