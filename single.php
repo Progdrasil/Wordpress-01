1ff9b7742c985874ee7008f5c771f0af581cf494
Rene Leveille <reneb.leveille@gmail.com>
2017-05-22 22:26:37 -0400

    show categories and posts

--- single.php
+++ single.php
@@ -17,6 +17,25 @@
         <div class="section group">
             <div class="col span_2_of_3">
             <?php
+                /* This is for categories and/or tags */
+            ?>
+            <section>
+                <p>
+                    Categories: 
+                    <?php
+                        the_category(', ');
+                    ?>
+                </p>
+                <p>
+                    Tags: 
+                    <?php
+                        the_tags('<div id="tags">',' - ','</div>');
+                    ?>
+                </p>
+
+            </section>
+            <?php
+                /* this is for posts */
                 while (have_posts()) : the_post();
                     the_content();
                 endwhile;
