211f754f692990edf6c0024039a7b558e97cab76
Rene Leveille <reneb.leveille@gmail.com>
2017-05-25 22:04:42 -0400

    adding images

--- search.php
+++ search.php
@@ -12,9 +12,11 @@
             echo "<h2>Search Results: ".get_search_query()."</h2>";
 
             while(have_posts()) : the_post();
+
+                echo '<a href="'.get_the_permalink().'">'.get_the_post_thumbnail(get_the_ID(),'thumbnail').'</a></br>';
                 echo '<p>'.get_the_title().'<br/>';
 
-                echo '<span style="font-size:10px; color:#999">'.get_the_date().'</span></br>';
+                echo '<span style="font-size:10px; color:#999">'.get_the_date().' - '.get_the_author().' - '.get_the_author_posts().'</span></br>';
 
                 $content = substr(strip_tags(get_the_content()),0,100);
                 echo $content.'</br>';
