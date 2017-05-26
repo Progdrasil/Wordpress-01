20797007d502146543d5cbc0e6849c14034d5939
Rene Leveille <reneb.leveille@gmail.com>
2017-05-25 20:28:27 -0400

    Search Form template file

---
+++ searchform.php
@@ -0,0 +1,20 @@
+<form class="searchform" action="<?php home_url('/') ?>" method="get">
+    Search : <input type="text" name="s" value="<?php echo the_search_query()?>"/><br/>
+
+    <select name="search_category">
+        <option name="none">-- Select a category --</option>
+        <?php
+            $categories = get_categories();
+            foreach($categories as $cat)
+            {
+                $val = $cat->category_nicename;
+                $name = $cat->cat_name;
+                $count = $cat->category_count;
+
+                print("<option value='$val'>$name($count)</option>");
+            }
+        ?>
+    </select>
+
+    <input type="submit" value="Search"/>
+</form>
