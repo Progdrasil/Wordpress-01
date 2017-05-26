85fe3021a42024efae6814a01e2419d29d582821
Rene Leveille <reneb.leveille@gmail.com>
2017-05-25 21:45:36 -0400

    populate search page

--- search.php
+++ search.php
@@ -10,6 +10,16 @@
         <div class="col span_2_of_3">
         <?php
             echo "<h2>Search Results: ".get_search_query()."</h2>";
+
+            while(have_posts()) : the_post();
+                echo '<p>'.get_the_title().'<br/>';
+
+                echo '<span style="font-size:10px; color:#999">'.get_the_date().'</span></br>';
+
+                $content = substr(strip_tags(get_the_content()),0,100);
+                echo $content.'</br>';
+                echo '<a href="'.get_the_permalink().'" style="font-size:12px;color:green;">'.get_the_permalink().'</a>'.'</p>';
+            endwhile;
         ?>
         </div>
         <div class="col span_1_of_3">
