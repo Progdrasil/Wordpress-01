012146041c8de5387c99c347dcd8bcce27dd3155
Rene Leveille <reneb.leveille@gmail.com>
2017-05-25 23:07:58 -0400

    tag cloud

--- sidebar.php
+++ sidebar.php
@@ -1,1 +1,16 @@
+<?php
+
+    $args = array(
+        'smallest'  => '10',
+        'largest'   => '20',
+        'unit'      => 'pt',
+        // 'number'    => '1',
+        'format'    => 'flat',
+        'separator' => ' | ',
+        'order'     => 'RAND',
+    );
+    wp_tag_cloud($args);
+
+?>
+
 the current year is <?php echo the_current_date(); ?>
