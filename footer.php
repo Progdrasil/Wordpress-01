57b0f910a1793b2a1e29b8ade274fa641a5a6559
Rene Leveille <reneb.leveille@gmail.com>
2017-05-26 21:41:53 -0400

    showing menus on page

--- footer.php
+++ footer.php
@@ -1,4 +1,15 @@
         <footer>
+            <?php
+                wp_nav_menu(
+                    array(
+                        'theme_location'    =>  'footer_menu',
+                        'fallback_cb'       =>  false,
+                        'menu_id'           =>  'footer-menu',
+                        'menu_class'        =>  'footer-menus',
+                        'after'             =>  ' |',
+                    )
+                );
+            ?>
             &copy; <?php echo the_current_date(); ?> Designed by Rene Leveille
         </footer>
         <?php wp_footer(); ?>
--- header.php
+++ header.php
@@ -6,3 +6,21 @@
 
         <?php wp_head(); ?>
     </head>
+    <body>
+        <?php
+            $header_menu = wp_nav_menu(
+                array(
+                    'theme_location'=>  'header_menu',
+                    'echo'          =>  false,
+                    'menu_class'    =>  'all-menus',
+                    'menu_id'       =>  'header-menu',
+                    'fallback_cb'   =>  false,
+                    'before'        =>  '<img src="http://college.wfu.edu/biology/wp-content/uploads/2013/07/link.png" style="width:16px;height:16px;"/>&nbsp&nbsp',
+                    'after'         =>  '|',
+                    'container'     =>  'div',
+                    'container_id'  =>  'header-parent',
+                    'container_class'=> 'header-menus'
+                )
+            );
+            echo $header_menu;
+        ?>
