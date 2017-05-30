c87b1f43c15f41b832d358e5d22cbd17935e464e
Rene Leveille <reneb.leveille@gmail.com>
2017-05-29 23:42:38 -0400

    force installation of site-origins

--- example.php
+++ example.php
@@ -70,6 +70,17 @@ function TestTheme_register_required_plugins() {
 			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
 			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
 		),
+		array(
+			'name'               => 'Page Builder by SiteOrigins', // The plugin name.
+			'slug'               => 'siteorigin-panels', // The plugin slug (typically the folder name).
+			'source'             => get_template_directory() . '/plugins/siteorigin-panels.2.5.3.zip', // The plugin source.
+			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+			'version'            => '2.5.3', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'force_activation'   => true, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_deactivation' => true, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+		),
 
 		// This is an example of how to include a plugin from an arbitrary external source in your theme.
 		array(
@@ -107,11 +118,11 @@ function TestTheme_register_required_plugins() {
 			'slug'        => 'wordpress-seo',
 			'is_callable' => 'wpseo_init',
 		),
-		array(
-			'name'		=>	'Page Builder by SiteOrigins',
-			'slug'		=>	'siteorigin-panels',
-			'required'	=>	true,
-		),
+		// array(
+		// 	'name'		=>	'Page Builder by SiteOrigins',
+		// 	'slug'		=>	'siteorigin-panels',
+		// 	'required'	=>	true,
+		// ),
 
 	);
 
