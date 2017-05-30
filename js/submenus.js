ebb72d54f5d224d54f80fa70ff1b58eee7c4057c
Rene Leveille <reneb.leveille@gmail.com>
2017-05-29 23:49:52 -0400

    reset theme

--- js/submenus.js
+++
@@ -1,9 +0,0 @@
-$('.submenu li').hide();
-$('#header-menu li > .sub-menu').parent().hover(
-    function(){
-        $(this).children('.sub-menu').slideDown(200);
-    },
-    function () {
-        $(this).children('.sub-menu').slideUp(200);
-    }
-);
--- searchform.php
+++
@@ -1,4 +0,0 @@
-<form class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get">
-    Search : <input type="text" name="s" value="<?php echo the_search_query()?>"/><br/>
-    <input type="submit" value="Search"/>
-</form>
